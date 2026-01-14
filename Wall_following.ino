8c0e6188afc58bb6a0b6573a26f073445b1710ff
Ojas <65460684+ojas-skyhigh@users.noreply.github.com>
2026-01-14 12:27:13 +0530

    Refactor motor control for speed adjustments
    Updated motor control functions to accept speed parameters and adjusted movement logic to use defined speeds.

--- Wall_following.ino
+++ Wall_following.ino
@@ -21,6 +21,13 @@ const int pwmResolution = 10;
 #define SOUND_SPEED 0.034
 #define WALL_DIST 10.00
 
+#define RIGHT_SPD 100 //larger the value, slower the speed
+#define LEFT_SPD 100
+#define RIGHT_TURN_SPD 512
+#define LEFT_TURN_SPD 512 //larger the value, slower the turn (also affects reverse speed)
+
+#define TARGET_LEFT_DIST 8.0   // center of corridor 
+#define KP 15.0                // proportional gain (TUNE)
 
 long duration;
 float distance;
@@ -69,8 +76,9 @@ void loop() {
         state = TURNING_RIGHT;
       }
       else {
-        moveForward(l_in1, l_in2);
-        moveForward(r_in1, r_in2);
+        // moveForward(l_in1, l_in2, LEFT_SPD);
+        // moveForward(r_in1, r_in2, RIGHT_SPD);
+        followLeftWall();
       }
       break;
 
@@ -78,8 +86,8 @@ void loop() {
       // Stop turning when left wall appears again
       if (L) {
         stopMotors();
-        moveForward(l_in1, l_in2);
-        moveForward(r_in1, r_in2);
+        moveForward(l_in1, l_in2, LEFT_SPD);
+        moveForward(r_in1, r_in2, RIGHT_SPD);
         state = MOVING_FORWARD;
       } else {
         turnLeft();
@@ -90,8 +98,8 @@ void loop() {
       // Stop turning when front becomes clear
       if (!F) {
         stopMotors();
-        moveForward(l_in1, l_in2);
-        moveForward(r_in1, r_in2);
+        moveForward(l_in1, l_in2, LEFT_SPD);
+        moveForward(r_in1, r_in2, RIGHT_SPD);
         state = MOVING_FORWARD;
       } else {
         turnRight();
@@ -99,12 +107,12 @@ void loop() {
       break;
   }
 
-  moveForward(l_in1, l_in2);
-  moveForward(r_in1, r_in2);
+  moveForward(l_in1, l_in2, LEFT_SPD);
+  moveForward(r_in1, r_in2, RIGHT_SPD);
 
   delay(3000);
-  reverseMotor(l_in1, l_in2);
-  reverseMotor(r_in1, r_in2);
+  reverseMotor(l_in1, l_in2, LEFT_TURN_SPD);
+  reverseMotor(r_in1, r_in2, RIGHT_TURN_SPD);
   delay(3000);
 }
 
@@ -116,7 +124,8 @@ bool isWall(int trigPin, int echoPin) {
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   
-  duration = pulseIn(echoPin, HIGH);
+  duration = pulseIn(echoPin, HIGH, 25000); // 25ms timeout
+  if (duration == 0) return false;
   
   distance = duration * SOUND_SPEED/2; // in cm
 
@@ -125,30 +134,43 @@ bool isWall(int trigPin, int echoPin) {
   return false;
 }
 
+float getDistance(int trigPin, int echoPin) {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  duration = pulseIn(echoPin, HIGH, 25000); // 25ms timeout
+  if (duration == 0) return 100.0;
+  distance = duration * SOUND_SPEED/2; // in cm
+  return distance;
+}
 
-void moveForward(int in1, int in2) {
+void moveForward(int in1, int in2, int spd) {
   digitalWrite(in1, HIGH);
   Serial.println("Forward");
 
-  ledcWrite(in2, 0);     // set a speed here
+  ledcWrite(in2, spd);     // set a speed here
 }
 
-void reverseMotor(int in1, int in2) {
+void reverseMotor(int in1, int in2, int spd) {
+  int reverse_spd = 1024-spd;
   digitalWrite(in1, LOW);  
   Serial.println("Reverse");
 
-  ledcWrite(in2, 512);
+  ledcWrite(in2, reverse_spd);
 }
 
 void turnLeft() {
-  moveForward(r_in1, r_in2);
-  reverseMotor(l_in1, l_in2);
+  moveForward(r_in1, r_in2, RIGHT_TURN_SPD);
+  reverseMotor(l_in1, l_in2, LEFT_TURN_SPD);
   Serial.println("Left");
 }
 
 void turnRight() {
-  moveForward(l_in1, l_in2);
-  reverseMotor(r_in1, r_in2);
+  moveForward(l_in1, l_in2, LEFT_TURN_SPD);
+  reverseMotor(r_in1, r_in2, RIGHT_TURN_SPD);
   Serial.println("Right");
 }
 
@@ -159,4 +181,21 @@ void stopMotors() {
 
   ledcWrite(l_in2, 0);     // Fast
   ledcWrite(r_in2, 0);
+}
+
+void followLeftWall() {
+  float leftDist = getDistance(l_trigPin, l_echoPin);
+
+  float error = TARGET_LEFT_DIST - leftDist;
+  float correction = KP * error;
+
+  int leftSpeed  = LEFT_SPD - correction;
+  int rightSpeed = RIGHT_SPD + correction;
+
+  // Clamp speeds
+  leftSpeed  = constrain(leftSpeed,  0, 1023);
+  rightSpeed = constrain(rightSpeed, 0, 1023);
+
+  moveForward(l_in1, l_in2, leftSpeed);
+  moveForward(r_in1, r_in2, rightSpeed);
 }
